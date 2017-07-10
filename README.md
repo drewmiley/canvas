fed011d857ae0c7482834e577ee1cca43e0c39d2
Drew Miley <drewmiley92@gmail.com>
2017-07-10 01:50:24 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Canvas
+
+Canvas manipulation using jquery setup with bootstrap, babel, and webpack.
+
+## Running the app
+
+The app uses webpack-dev-server to aid development.
+
+To start the development server with hot reloading enabled, simply run
+
+```
+npm start
+```
+
+## Deploying the app
+
+To deploy the app to gh-pages, there are a couple of steps to follow.
+
+1. Run `webpack` command. This will create a `bundle.js` and `bundle.js.map` file in the dist folder.
+2. Delete everything except dist folder.
+3. Empty contents of dist folder to root directory.
+4. Commit and push to gh-pages.
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var webpack = require('webpack');
+
+module.exports = {
+	cache: true,
+	debug: true,
+	devtool: 'source-map',
+	entry: [
+		'webpack-dev-server/client?http://localhost:8080',
+		'webpack/hot/only-dev-server',
+		'./src/app.js'
+	],
+	module: {
+		loaders: [{
+			test: /\.js?$/,
+			exclude: /node_modules/,
+			loader: 'babel',
+			query: {
+				presets: ['es2015']
+			}
+		}]
+	},
+	output: {
+		path: __dirname + '/dist',
+		publicPath: '/',
+		filename: 'bundle.js'
+	},
+	devServer: {
+		contentBase: './dist',
+		hot: true
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.ProvidePlugin({
+			'$': 'jquery',
+			'jQuery': 'jquery',
+			'window.jQuery': 'jquery'
+		}),
+	]
+};
